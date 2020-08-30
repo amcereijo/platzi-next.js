7145776329b2f2da5bd0cdd33b61eab7eae30383
amcereijo <amcereijo@gmail.com>
2020-08-30 11:17:44 +0200

    server-seide rendering and more

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,21 @@ import React, { useState, useEffect } from 'react'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
+import fetch from 'isomorphic-unfetch'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+export async function getServerSideProps(/* params */) {
+  const response: TAPIAvoResponse = await fetch(
+    `${process.env.API_HOST}/api/avo`
+  ).then((response) => response.json())
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+  return {
+    props: {
+      productList: response.data,
+    },
+  }
+}
 
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
